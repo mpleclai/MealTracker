d9ef63cecbab22a5fa81601fd342de23fe7b451d
mpleclai <mpleclai@mtu.edu>
2018-08-07 19:39:52 -0500

    First commit

---
+++ src/Calendar.kt
@@ -0,0 +1,6 @@
+fun main( args : Array< String>) {
+    val days = arrayOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+   for( i in days){
+       println(i)
+   }
+}
